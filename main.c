549cdbb1547103440bdaedc92d3e61383d189dce
yu-432 <o.shimayu891@gmail.com>
2024-12-01 13:11:13 +0900

    :+1: 直接光反射

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 16:18:28 by yooshima          #+#    #+#             */
-/*   Updated: 2024/11/30 16:14:47 by yooshima         ###   ########.fr       */
+/*   Updated: 2024/12/01 13:07:58 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,9 @@
 # define WIDTH 800
 # define HEIGHT 800
 
-// bool hit_sphere(t_vec *Camera, t_vec *Sphere, double sp_size)
-// {
-	
-// }
-
-// t_vec	trans_coord(t_vec *vec)
-// {
-// 	t_vec res;
-
-// 	res.x = 
-// }
-
+# define AMBIENT_REFLECTION 0.1
+# define DIFFUSE_REFLECTION 0.6
+# define SPECULAR_REFLECTION 0.3
 
 int create_trgb(int t, int r, int g, int b)
 {
@@ -44,15 +35,29 @@ void normalize(t_vec *vec)
 	vec->z = vec->z / vec_size;
 }
 
+void my_pixel_put(t_data *data, int x, int y , int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void *mlx;
 	void *win;
+	t_data img;
 	double t;
 	double sp_size = 1.0;
+	double ambient_light = 0.2;//ambient lighting ratio
+	double luminance_ratio = 0.6;
+	double ambient_radiance_ratio;
+	double luminance_radiance_ratio;
+	double direct_light_radiance;
 	t_vec Sphere = {0, 0, 5};
-	t_vec Camera = {0, 0, -5};
-	t_vec Light = {-5, 5 , -5};
+	t_vec Camera = {0, 0, -5};//FOV, 方向ベクトル が存在する
+	t_vec Light = {-5, 5 , -5};//輝度比light brightness ratio
 	t_vec pw;
 	t_vec *intersection_pos;
 	t_vec *incident_ray;
@@ -60,6 +65,9 @@ int	main(void)
 	double inner;
 
 	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	
 	win = mlx_new_window(mlx, WIDTH, HEIGHT, "miniRT");
 	for (int i = 0; i < WIDTH; i++)
 	{
@@ -96,15 +104,36 @@ int	main(void)
 				normalize(incident_ray);
 				normal_vec = vec_sub(intersection_pos, &Sphere);
 				normalize(normal_vec);
-				inner = vec_dot(incident_ray, normal_vec);
+				inner = vec_dot(incident_ray, normal_vec);//内積
 				if (inner <= 0)
 					inner = 0;
-				mlx_pixel_put(mlx, win, i, j, create_trgb(0, 255 * inner, 255 * inner, 255 * inner));
+
+				//ここから2-5-2-2
+				ambient_radiance_ratio = AMBIENT_REFLECTION * ambient_light;
+				luminance_radiance_ratio = DIFFUSE_REFLECTION * luminance_ratio * inner;//???
+				
+				if (inner > 0)
+				{
+					t_vec *orthorectified_vec = \
+						vec_sub(vec_mult(normal_vec, vec_dot(normal_vec, incident_ray) * 2), incident_ray);
+
+					t_vec *inverse_vec = vec_mult(&Camera, -1);
+					normalize(inverse_vec);
+					inner = vec_dot(inverse_vec, orthorectified_vec);
+					// normalize(inner);
+					direct_light_radiance = SPECULAR_REFLECTION * 1.0 //光源の光の強度
+						* pow(vec_dot(inverse_vec, orthorectified_vec), 8);
+				}
+				else
+					direct_light_radiance = 0;
+				double radiance_reflected_light = ambient_radiance_ratio + luminance_radiance_ratio + direct_light_radiance;
+				my_pixel_put(&img, i, j, create_trgb(0, 230 * radiance_reflected_light, 255 * radiance_reflected_light, 255 * radiance_reflected_light));
 			}
 			else
-				mlx_pixel_put(mlx, win, i, j, 100);
+				my_pixel_put(&img, i, j, create_trgb(0, 140, 220, 230));
 		}
 	}
+	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:22:53 by yooshima          #+#    #+#             */
-/*   Updated: 2024/11/30 12:45:06 by yooshima         ###   ########.fr       */
+/*   Updated: 2024/11/30 19:25:51 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,15 @@ typedef struct s_vec
 	double y;
 	double z;
 }	t_vec;
+
+typedef struct s_img
+{
+	void *img;
+	char *addr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+}	t_data;
 
 t_vec	*vec_add(t_vec *a, t_vec *b);
 t_vec	*vec_sub(t_vec *a, t_vec *b);
